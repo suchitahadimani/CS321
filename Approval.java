be1b635a553dbd9889ae87454550b0a5c9770bf3
SadhanaMa <smallemu@gmu.edu>
2023-11-01 22:42:55 -0400

    basic ui

--- Approval.java
+++ Approval.java
@@ -1,4 +1,105 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+
 public class Approval {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Account Creation");
+        Text scenetitle = new Text("Welcome! Get started on making your account!");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+
+        Label firstname = new Label("First Name:");
+        Label lastname = new Label("Last Name:");
+        Label dob = new Label("Date of birth:");
+        Label address = new Label("Address:");
+        Label city = new Label("City:");
+        Label state = new Label("State:");
+        Label zipcode = new Label("Zip-Code:");
+        Label email = new Label("Email:");
+        Label phone_number = new Label("Phone Number:");
+
+        grid.add(firstname, 0, 1);
+        grid.add(lastname, 0, 2);      
+        grid.add(dob, 0, 3);                          
+        grid.add(address, 0, )4;                                   
+        grid.add(city, 0, );                                    
+        grid.add(state, 0, 5);                                    
+        grid.add(zipcode, 0, 6);                                    
+        grid.add(email, 0, 7);                                   
+        grid.add(phone_number, 0, 8);
+
+
+        Label education = new Label("Enter highest education completed:");
+        Label job = new Label("List of Job:");
+        Label documents = new Label("Upload identification documents:");
+
+
+        Label tf_firstname = new Label("First Name:");
+        Label tf_lastname = new Label("Last Name:");
+        Label tf_address = new Label("Address:");
+        Label tf_city = new Label("City:");
+        Label tf_state = new Label("State:");
+        Label tf_zipcode = new Label("Zip-Code:");
+        Label tf_email = new Label("Email:");
+
+        grid.add(tf_firstname, 1, 1);
+        grid.add(tf_astname, 1, 2);              
+        grid.add(tf_dob, 1, 3);                              
+        grid.add(tf_address, 1, )4;                                    
+        grid.add(tf_city, 1, );                                   
+        grid.add(tf_state, 1, 5);                                   
+        grid.add(tf_zipcode, 1, 6);                                    
+        grid.add(tf_email, 1, 7);                                    
+        grid.add(tf_phone_number, 1, 8);
+
+
+        Button approve = new Button("APPROVE");
+        approve.setOnAction(e -> {
+            approve.setFill(Color.FIREBRICK);
+            System.out.println("Button Clicked!");
+        });
+
+        Button deny = new Button("DENY");
+        deny.setOnAction(e -> {
+            deny.setFill(Color.FIREBRICK);
+            System.out.println("Button Clicked!");
+        });
+
+
+        grid.add(approve);
+        grid.add(deny);
+
+        Scene scene = new Scene(grid, 300, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    
     private DataEntry immigrant;
     private int alienNumber;
 
@@ -22,6 +123,7 @@ public class Approval {
         return true;
     }
 
+<<<<<<< HEAD
     public boolean displayScreen() {
         return true;
     }
@@ -30,6 +132,8 @@ public class Approval {
         return true;
     }
 
+=======
+>>>>>>> b4c0cabed04ee33fd4081e66e3ad79462881b092
     public boolean deny(int alienNumber) {
         return false;
     }
--- DataEntry.java
+++ DataEntry.java
@@ -1,23 +1,154 @@
-import java.io.File;
-public class DataEntry {
 
-    private String name;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+
+import java.io.File;
+
+public class DataEntry extends Application{
+
+    private String firstname;
+    private String lastname;
     private String address;
     private String email;
-    private int ssn;
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
     private int dob;
     private int phone_number;
     private String[] job;
     private String[] education;
-    private File[] files;
-
-    // Getter and setter for 'name'
-    public String getName() {
-        return name;
+    private File[] files;       
+    
+    //to launch application
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Account Creation");
+        Text scenetitle = new Text("Welcome! Get started on making your account!");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+
+
+        Label l_firstname = new Label("First Name:");
+        Label l_lastname = new Label("Last Name:");
+        Label l_dob = new Label("Date of birth:");
+        Label l_address = new Label("Address:");
+        Label l_city = new Label("City:");
+        Label l_state = new Label("State:");
+        Label l_zipcode = new Label("Zip-Code:");
+        Label l_email = new Label("Email:");
+        Label l_phone_number = new Label("Phone Number:");
+
+        grid.add(l_firstname, 0, 1);
+        grid.add(l_lastname, 0, 2);      
+        grid.add(l_dob, 0, 3);                          
+        grid.add(l_address, 0, )4;                                   
+        grid.add(l_city, 0, );                                    
+        grid.add(l_state, 0, 5);                                    
+        grid.add(l_zipcode, 0, 6);                                    
+        grid.add(l_email, 0, 7);                                   
+        grid.add(l_phone_number, 0, 8);
+
+
+        Label education = new Label("Enter highest education completed:");
+        Label job = new Label("List of Job:");
+        Label documents = new Label("Upload identification documents:");
+
+
+        TextField tf_firstname = new TextField("First Name:");
+        TextField tf_lastname = new TextField("Last Name:");
+        TextField tf_address = new TextField("Address:");
+        TextField tf_city = new TextField("City:");
+        TextField tf_state = new TextField("State:");
+        TextField tf_zipcode = new TextField("Zip-Code:");
+        TextField tf_email = new TextField("Email:");
+
+        grid.add(tf_firstname, 1, 1);
+        grid.add(tf_astname, 1, 2);              
+        grid.add(tf_dob, 1, 3);                              
+        grid.add(tf_address, 1, )4;                                    
+        grid.add(tf_city, 1, );                                   
+        grid.add(tf_state, 1, 5);                                   
+        grid.add(tf_zipcode, 1, 6);                                    
+        grid.add(tf_email, 1, 7);                                    
+        grid.add(tf_phone_number, 1, 8);
+
+
+
+
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 10);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 10);
+
+
+
+        Button btn = new Button("SUBMIT");
+        btn.setOnAction(e -> {
+            btn.setFill(Color.YELLOW);
+            System.out.println("Button Clicked!");
+            setFirstName(l_firstname.getText());
+            setLastName(l_lastname.getText());
+            setAddress(l_address.getText());
+            setCity(l_city.getText());
+            setState(l_state.getText());
+            setZipcode(l_zipcode.getText());
+            setEmail(l_email.getText());
+            setPhoneNumber(l_phone_number.getText());
+        });
+
+        
+        grid.add(btn);
+
+
+        Scene scene = new Scene(grid, 300, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+
+
+    // Getter and setter for 'name'
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+
+    // Getter and setter for 'name'
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
     }
 
     // Getter and setter for 'address'
@@ -29,6 +160,34 @@ public class DataEntry {
         this.address = address;
     }
 
+
+    // Getter and setter for 'address'
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    // Getter and setter for 'address'
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    // Getter and setter for 'address'
+    public String getZipecode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
     // Getter and setter for 'email'
     public String getEmail() {
         return email;
@@ -36,15 +195,6 @@ public class DataEntry {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    // Getter and setter for 'ssn'
-    public int getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.ssn = ssn;
     }
 
     // Getter and setter for 'dob'
@@ -96,10 +246,13 @@ public class DataEntry {
     }
 
 
+<<<<<<< HEAD
     boolean displayScreen(){
         return true;
     }
 
 
 
+=======
+>>>>>>> b4c0cabed04ee33fd4081e66e3ad79462881b092
 }
